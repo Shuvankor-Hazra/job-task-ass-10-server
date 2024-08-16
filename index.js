6e790c07498983fe5843f73b75c47bfdcaf491f4
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-08-16 13:09:45 +0600

    pagination added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// require
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -8,20 +9,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-const corsOptions = {
-  origin: ["http//localhost:5173", "http//localhost:5174"],
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 app.use(express.json());
-app.use(cors(corsOptions));
-
-
+app.use(cors());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fi65pdm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -32,14 +26,48 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // all collections
+    const database = client.db("luxury-pen");
+    const penCollection = database.collection("pen");
+
+    // all pen related api
+    app.get('/pens', async (req, res) => {
+      const result = await penCollection.find().toArray();
+      res.send(result);
+    })
+
+        // Get all books data from DB for pagination
+        app.get("/all-pens", async (req, res) => {
+          const size = parseInt(req.query.size);
+          const page = parseInt(req.query.page) - 1;
+          const filter = req.query.filter;
+          const sort = req.query.sort;
+          let query = {};
+          if (filter) query = { category: filter };
+          let options = {};
+          if (sort) options = { sort: { rating: sort === "ascending" ? 1 : -1 } };
+          const result = await penCollection
+            .find(query, options)
+            .skip(page * size)
+            .limit(size)
+            .toArray();
+          res.send(result);
+        });
+    
+        // Get all books data count from DB
+        app.get("/pens-count", async (req, res) => {
+          const filter = req.query.filter;
+          let query = {};
+          if (filter) query = { category: filter };
+          const count = await penCollection.countDocuments(query);
+          res.send({ count });
+        });
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
@@ -47,6 +75,7 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
     res.send("Job task is running.......");
   });
+
   app.listen(port, () => {
     console.log(`Server running on the port, ${port}`);
   });
